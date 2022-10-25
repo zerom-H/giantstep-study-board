8c93f712abcdfddee95314ad1281ffef11fd30bc
leejjback <116611945+leejjback@users.noreply.github.com>
2022-10-25 11:33:40 +0900

    TEST (#2)
    * TEST
    
    Co-authored-by: 이정준 <101760155+leejengjun@users.noreply.github.com>

--- src/main/java/com/giantstep/board/BoardApplication.java
+++ src/main/java/com/giantstep/board/BoardApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardApplication {
-	//test
+	//test 수정 추가
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
