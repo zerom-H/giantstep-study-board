794f419ae6b5c7ebdcc6b0a854cadd646ef137c9
이정준 <101760155+leejengjun@users.noreply.github.com>
2022-10-25 11:31:35 +0900

    TEST

--- src/main/java/com/giantstep/board/BoardApplication.java
+++ src/main/java/com/giantstep/board/BoardApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardApplication {
-	//test 수정
+	//test 수정 추가
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
