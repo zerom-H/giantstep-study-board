f60854978e0a155f90f527633e42f209a566cddc
이정준 <101760155+leejengjun@users.noreply.github.com>
2022-11-02 11:41:22 +0900

    Feat: "생성 Board Entity"
    게시판 관련된 Entity
    - Board (com.giantstep.board.domain.board.entity)
    를 해당 경로에 생성했습니다.
    연관관계와 테이블 매핑, 각 컬럼에 관련하여 작업을 해주었습니다.
    이후에 기능 구현할 때에 @Bulider나 생성자를 추가할 예정입니다.
    
    Resolve: giantstep-study-board #19

---
+++ src/main/java/com/giantstep/board/domain/board/entity/Board.java
@@ -0,0 +1,41 @@
+package com.giantstep.board.domain.board.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "board")
+@Entity
+public class Board {
+
+    /** 게시 글 번호 */
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_no")
+    private Long no;
+    
+    /** 작성자 */
+    @Column(name = "board_writer", columnDefinition = "varchar(100)", nullable = false)
+    private String writer;
+
+    /** 글 제목 */
+    @Column(name = "board_title", nullable = false)
+    private String title;
+
+    /** 글 내용 */
+    @Column(name = "board_contents", columnDefinition = "text", nullable = false)
+    private String contents;
+
+    /** 글 비밀번호 */
+    @Column(name = "board_password", nullable = false)
+    private int password;
+
+    @OneToMany(mappedBy = "board")
+    private List<BoardComment> boardComments = new ArrayList<>();
+
+}
