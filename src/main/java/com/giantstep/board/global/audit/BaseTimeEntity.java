1d169eafcfa541dc483fc354c7791a2416416852
이정준 <101760155+leejengjun@users.noreply.github.com>
2022-11-02 11:45:10 +0900

    Feat: "생성 BaseTimeEntity Entity"
    게시판 관련된 Entity
    - BaseTimeEntity (com.giantstep.board.global.audit)
    를 해당 경로에 생성
    작성일과 수정일은 Audit 기능을 활용
    
    Resolve: giantstep-study-board #19

---
+++ src/main/java/com/giantstep/board/global/audit/BaseTimeEntity.java
@@ -0,0 +1,30 @@
+package com.giantstep.board.global.audit;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class BaseTimeEntity {
+
+    /** 생성일 */
+    @CreatedDate
+    @DateTimeFormat(pattern = "yyyy-MM-DD_HH:mm:ss")
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createDate;
+
+    /** 수정일 */
+    @LastModifiedDate
+    @DateTimeFormat(pattern = "yyyy-MM-DD_HH:mm:ss")
+    @Column(nullable = false)
+    private LocalDateTime updateDate;
+}
