34fce6e00e3c11fb538fddf4d25ff1c2706a1af0
이정준 <101760155+leejengjun@users.noreply.github.com>
2022-11-07 12:05:12 +0900

    Feat: 추가 게시 물 작성 기능에 검증(validate)
    게시 물 작성 기능에 요구사항에 정의된 검증부분 반영
    - messages.properties 에 게시 물 관련 message 작성
    - boardAddForm.html 에 검증 부분 추가
    - BoardAddFormDto 에 검증 관련 어노테이션 설정
    - BoardController 에 게시 물 작성 요청 부분에서 검증 부분 추가
    
    Resolve: giantstep-study-board #5

--- src/main/java/com/giantstep/board/domain/board/controller/BoardController.java
+++ src/main/java/com/giantstep/board/domain/board/controller/BoardController.java
@@ -5,10 +5,13 @@ import com.giantstep.board.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,13 +23,20 @@ public class BoardController {
 
     /** 게시 물 작성 */
     @GetMapping("/boardAddForm")
-    public String boardAddForm(@ModelAttribute("BoardAddFormDto") BoardAddFormDto form){
+    public String boardAddForm(@ModelAttribute("board") BoardAddFormDto form){
         return "board/boardAddForm";
     }
 
     /** 게시 물 작성 완료 */
     @PostMapping("/boardAddForm")
-    public String boardwriteDone(@ModelAttribute BoardAddFormDto boardAddFormDto) {
+    public String boardwriteDone(@Valid @ModelAttribute("board") BoardAddFormDto boardAddFormDto, BindingResult bindingResult) {
+
+        //검증 실패하면 다시 작성 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {} ", bindingResult);
+            return "board/boardAddForm";
+        }
+
         boardService.saveBoard(boardAddFormDto);
         return "redirect:/giantstep-study/board";
     }
--- src/main/java/com/giantstep/board/domain/board/controller/dto/BoardAddFormDto.java
+++ src/main/java/com/giantstep/board/domain/board/controller/dto/BoardAddFormDto.java
@@ -1,23 +1,29 @@
 package com.giantstep.board.domain.board.controller.dto;
 
 import com.giantstep.board.domain.board.entity.Board;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
 public class BoardAddFormDto {
 
     private Long id;
-    private String writer;
-    private String title;
-    private String contents;
-    private String password;
 
-    private LocalDateTime createDate;
+    @NotBlank(message = "작성자는 공백일 수 없습니다.")
+    private String writer;
+
+    @NotBlank(message = "글 제목은 공백일 수 없습니다.")
+    private String title;
+
+    @NotBlank(message = "글 내용은 공백일 수 없습니다.")
+    private String contents;
+
+    @Pattern(regexp = "[0-9]{4}", message = "비밀번호는 숫자조합으로 4자리 입력하셔야 합니다.")
+    private String password;
 
     public Board toEntity() {
         Board board = Board.builder()
