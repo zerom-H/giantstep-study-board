1fee68a904a96b527f91d0540c14e5e228f86633
이정준 <101760155+leejengjun@users.noreply.github.com>
2022-11-08 12:45:01 +0900

    Feat: 추가 게시 물 작성 기능 관련 테스트 코드
    게시 물 작성에 대한 테스트 코드 작성
    BoardServiceTest 생성하여 게시 물 작성 테스트 코드 추가
    
    Resolve: giantstep-study-board #5

---
+++ src/test/java/com/giantstep/board/domain/board/service/BoardServiceTest.java
@@ -0,0 +1,40 @@
+package com.giantstep.board.domain.board.service;
+
+import com.giantstep.board.domain.board.entity.Board;
+import com.giantstep.board.domain.board.repository.BoardRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class BoardServiceTest {
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Test
+    public void 게시물_작성() {
+
+        //give
+        Board insert_board = new Board(31L, "이정준", "테스트 작성", "게시 물 저장 테스트 입니다.", "1234");
+
+        //when
+        Long saveBoardId = boardRepository.save(insert_board).getId();
+
+        //then
+        Board findSaveBoard = boardRepository.findById(saveBoardId).get();
+        assertEquals(insert_board.getId(), findSaveBoard.getId());
+        assertEquals(insert_board.getWriter(), findSaveBoard.getWriter());
+        assertEquals(insert_board.getTitle(), findSaveBoard.getTitle());
+        assertEquals(insert_board.getContents(), findSaveBoard.getContents());
+        assertEquals(insert_board.getPassword(), findSaveBoard.getPassword());
+
+    }
+}
