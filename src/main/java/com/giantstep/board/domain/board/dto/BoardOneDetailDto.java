4f8bed12d6a8d3ed0568c08d27d31712a3980fdc
이정준 <101760155+leejengjun@users.noreply.github.com>
2022-11-10 18:05:55 +0900

    Feat: 구현 게시 글 단건 조회
    게시 글 조회 기능 중 단건 조회 부분 구현.
    - boardOneDetail.html 뷰 생성.
    - messages.properties 에 게시 물 상세 조회 관련 추가.
    - BoardOneDetailDto 생성.
    - BoardController, BoardService 에 관련 기능 추가.
    - BoardServiceTest 게시 물 단건 조회 테스트 코드 작성.
    추가로 게시 물 전체 조회도 스프링 데이터 JPA 로 구현.
    
    Resolve: giantstep-study-board #4

---
+++ src/main/java/com/giantstep/board/domain/board/dto/BoardOneDetailDto.java
@@ -0,0 +1,28 @@
+package com.giantstep.board.domain.board.dto;
+
+import com.giantstep.board.domain.board.entity.Board;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class BoardOneDetailDto {
+
+    private Long boardId;
+    private String boardWriter;
+    private String boardTitle;
+    private String boardContents;
+    private LocalDateTime boardCreateDate;
+    private LocalDateTime boardUpdateDate;
+
+    public BoardOneDetailDto(Board board) {
+        this.boardId = board.getId();
+        this.boardWriter = board.getWriter();
+        this.boardTitle = board.getTitle();
+        this.boardContents = board.getContents();
+        this.boardCreateDate = board.getCreateDate();
+        this.boardUpdateDate = board.getUpdateDate();
+    }
+}
